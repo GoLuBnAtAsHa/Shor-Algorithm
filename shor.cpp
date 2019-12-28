5f4b0639e18cd0253c9a51f12c93f1099a4f15dd
Khalid Filali <56809036+KhalidFilali@users.noreply.github.com>
2019-12-28 00:10:33 -0500

    Create shor.cpp

---
+++ shor.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int gcd(int a, int b){
+   if (b == 0)
+   return a;
+   return gcd(b, a % b); 
+	}
+int main(){
+int n;
+	cin>>n;
+	float x=rand()%n;
+	cout<<x<<<endl;
+	int r=0;
+	
+	while(pow(x,r)!=1){
+		r++;
+	}
+	cout<<"The period is: "<<r<<endl;
+	float q;
+	q=pow(x,r/2)+1;
+	double o;
+	float e=pow(q,r/2)+1;
+	float f=pow(x,r/2)-1;
+		q=gcd(e,n);
+	o=gcd(f,n);
+	if(q* o == n&&q > 1&&q*o> 1)
+		cout<<q<<"*"<<o<<"="<<n<<endl;
+	if(r%2!=0){
+		cout<<"Number is odd, restart"<<endl;
+	return 0;
+	}
+ return 0;
+}
